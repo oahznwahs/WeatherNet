9972f82167c8189ce9042228d5e6d410a7d20762
oahznwahs <56942310+oahznwahs@users.noreply.github.com>
2020-05-27 18:25:06 -0400

    Update main.js

--- WeatherNet/js/main.js
+++ WeatherNet/js/main.js
@@ -1,12 +1,9 @@
 /*
-  请求地址:http://wthrcdn.etouch.cn/weather_mini
-  请求方法:get
-  请求参数:city(城市名)
-  响应内容:天气信息
+  request url:http://wthrcdn.etouch.cn/weather_mini
+  request method:get
+  request parameter:city
+  response:weather info
 
-  1. 点击回车
-  2. 查询数据
-  3. 渲染数据
   */
  var app = new Vue({
      el:"#app",
@@ -16,10 +13,6 @@
      },
      methods: {
          searchWeather:function(){
-            //  console.log('天气查询');
-            //  console.log(this.city);
-            // 调用接口
-            // 保存this
             var that = this;
             axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
             .then(function(response){
